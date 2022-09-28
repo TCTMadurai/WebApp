799d5a77668cf349889757b0ecb68edb335d81fa
TCTMadurai <saravanankarthick09@gmail.com>
2022-09-28 17:17:39 +0530

    sound Changes committed

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -13,6 +13,11 @@ namespace WebApplication1.Controllers
             return View();
         }
 
+        public ActionResult SoundTest()
+        {
+            ViewBag.Message = "Sound Test Demo";
+            return View();
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
